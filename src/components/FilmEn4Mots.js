70b3e6c565c0216c9b5a1d7ddb1c7faba3edc340
Fradet <fradetaxel@outlook.com>
2021-12-01 21:08:38 +0100

    Film 4 mots WIP #2

--- src/components/FilmEn4Mots.js
+++ src/components/FilmEn4Mots.js
@@ -3,29 +3,47 @@ import Mot from "./Mot";
 import edit from "../assets/edit.svg";
 import undo from "../assets/undo.svg";
 import forward from "../assets/forward.svg";
-import reset from "../assets/reset.svg";
+import resetIcon from "../assets/reset.svg";
 
 const FilmEn4Mots = () => {
+  const [isEdited, setEdited] = useState(false);
+  const [show, setShow] = useState(1);
+  const cptWord = ["", "", "", ""];
+
+  const listWord = cptWord.map((el, id) => (
+    <Mot key={id} id={id} isEdited={isEdited} show={show} />
+  ));
+
+  function upgradeShow() {
+    show > 3 ? null : setShow(show + 1);
+  }
+  function downShow() {
+    show == 1 ? null : setShow(show - 1);
+  }
+
   return (
     <div className="main-film-container">
       <span className="main-police-title ">FILM EN 4 MOTS</span>
       <div className="main-film-screen">
-        <div className="main-film-screen-container">
-          <Mot mot={"FILM"} />
-          <Mot mot={"ELEPHANT"} />
-          <Mot mot={"CROCODILE"} />
-          <Mot mot={"OUI"} />
-        </div>
+        <div className="main-film-screen-container">{listWord}</div>
       </div>
       <div className="main-film-settings">
         <div className="main-film-settings-container">
           <img
             src={edit}
+            onClick={() => setEdited(!isEdited)}
             style={{ cursor: "pointer", marginLeft: "15px", marginTop: "3px" }}
           />
-          <img src={undo} style={{ cursor: "pointer" }} />
-          <img src={forward} style={{ cursor: "pointer" }} />
-          <img src={reset} style={{ cursor: "pointer", marginRight: "15px" }} />
+          <img
+            src={undo}
+            style={{ cursor: "pointer" }}
+            onClick={() => downShow()}
+          />
+          <img
+            src={forward}
+            style={{ cursor: "pointer", marginRight: "15px" }}
+            onClick={() => upgradeShow()}
+          />
         </div>
       </div>
     </div>
--- src/components/Mot.js
+++ src/components/Mot.js
@@ -1,10 +1,36 @@
+import React, { useState } from "react";
+import iconDelete from "../assets/delete.svg";
+
 const Mot = (props) => {
-  const { mot } = props;
-  return (
-    <div className="main-mot-container">
-      <span className="main-mot-police">{mot}</span>
-    </div>
-  );
+  const [word, setWord] = useState("");
+  let { id, show, isEdited, reset, setReset } = props;
+
+  if (show > id) {
+    return (
+      <div className="main-mot-container">
+        {isEdited ? (
+          <div className="main-mot-edit-container">
+            <input
+              className="main-mot-police-input"
+              value={word}
+              onChange={(e) => {
+                setWord(e.target.value);
+              }}
+            />
+            <img
+              src={iconDelete}
+              style={{ cursor: "pointer" }}
+              onClick={() => setWord("")}
+            />
+          </div>
+        ) : (
+          <span className="main-mot-police">{word}</span>
+        )}
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Mot;
